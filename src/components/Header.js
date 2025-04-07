809886711750c385bcc1c5815050e08abb096dc5
Mauricio Rivadeneira <tu-email@ejemplo.com>
2025-04-06 20:55:33 -0500

    Agregar logo al Header y actualizar estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">
-          <h1>CHE - Corporación Herejía Económica</h1>
-        </Link>
+    <header className="header flex items-center justify-between p-4">
+      <div className="logo-container">
+        <a href="/" className="flex items-center">
+          <img 
+            src="/images/logo.png" 
+            alt="CHE - Corporación Herejía Económica" 
+            className="logo h-12 w-auto object-contain"
+            style={{
+              maxHeight: '80px',
+              maxWidth: '200px'
+            }}
+          />
+        </a>
       </div>
       <nav className="main-nav">
-        <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/fondos">Fondos Rotatorios</Link></li>
-          <li><Link to="/about">Nosotros</Link></li>
-          <li><Link to="/contact">Contacto</Link></li>
+        <ul className="flex space-x-4">
+          <li><a href="/" className="text-white hover:text-gray-200">Inicio</a></li>
+          <li><a href="/fondos" className="text-white hover:text-gray-200">Fondos Rotatorios</a></li>
+          <li><a href="/about" className="text-white hover:text-gray-200">Nosotros</a></li>
+          <li><a href="/contact" className="text-white hover:text-gray-200">Contacto</a></li>
         </ul>
       </nav>
     </header>
