74265b2fd928910aac997ca1248e5571fd6ebea6
Mauricio Rivadeneira <tu-email@ejemplo.com>
2025-04-27 09:52:28 -0500

    Corregir configuración para despliegue en Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
+module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.js',
   output: {
