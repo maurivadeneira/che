0318d59bedc9e7d72ce74fd7cf238cd9b57d822c
Mauricio Rivadeneira <maurivadeneira@yahoo.es>
2025-05-04 09:56:40 -0500

    Corregir últimas importaciones .js en App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from '../layout/Layout.js';
-import Home from '../pages/Home.js';
+import Layout from '../layout/Layout.jsx';
+import Home from '../pages/Home.jsx';
 import FondosRoutes from '../fondos/routes.jsx';
 import AboutPage from '../pages/AboutPage.js';
 import ContactPage from '../pages/ContactPage.js';
