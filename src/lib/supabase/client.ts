a2b0ddb4d74de48cfc42a9a3e5687f5bfe79d5fa
Mauricio Rivadeneira <maurivadeneira@yahoo.es>
2025-11-20 11:10:05 -0500

    Revert: Volver a usar createClientComponentClient

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,14 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export function getSupabaseClient() {
-  // Hardcodeado temporalmente para debug
-  const supabaseUrl = 'https://cthwfjcgqo21bcuagsfem.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aHdmamNncW8yMWJjdWFnc2ZlbSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzMyNjg1NDM2LCJleHAiOjIwNDgyNjE0MzZ9.YWN1YWdzZmttlwcm9ZS16ImF1bZ4LlCJpYXQ1OjE3MjIyNlYyMDIzaEwAcCTe0M3ANz';
-
-  return createClient(supabaseUrl, supabaseKey, {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-    },
-  });
+  return createClientComponentClient();
 }
